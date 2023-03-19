540c0e18bc9285d5cc586f77b41d3ae0007f8078
Kainã Bermudes <bermudes13@outlook.com>
2023-03-19 14:51:11 +0000

    Update ControleEditora.tsx

--- public/controle/ControleEditora.tsx
+++ public/controle/ControleEditora.tsx
@@ -1,5 +1,5 @@
 import Editora from '../modelo/Editora';
-const baseURL = `https://projeto-facu-nv3.vercel.app/editora.json`
+const baseURL = `https://missao-nivel3-mundo2-5dhl.vercel.app/editora.json`
 
  async function getEditoras(): Promise<Array<Editora>> {
   const response = await fetch(baseURL);
